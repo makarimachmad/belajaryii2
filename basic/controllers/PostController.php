bde205433703d06b7a7767a91e5927f2a812275a
achmad.widyanto <achmad.widyanto@spesolution.com>
2021-12-13 12:31:46 +0700

    controller for post model

---
+++ basic/controllers/PostController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\Post;
+use yii\rest\ActiveController;
+
+class PostController extends ActiveController
+{
+    public $modelClass = Post::class;
+}
