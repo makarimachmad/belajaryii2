e0bd21b3bb676da2e2e4abe4ec1b4e34cc85d50b
achmad.widyanto <achmad.widyanto@spesolution.com>
2021-12-13 17:22:58 +0700

    fields active post data

---
+++ basic/resources/Post.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\resources;
+
+class Post extends \app\models\Post
+{
+	public function fields(){
+		return ['id', 'title', 'body'];
+	}
+
+	public function extraFields(){
+		return ['created_at', 'updated_at', 'updated_by'];
+	}
+}
