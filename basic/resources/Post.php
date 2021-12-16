b4941d7726eb2a5081076d018425f19dd167e19a
achmad.widyanto <achmad.widyanto@spesolution.com>
2021-12-16 08:55:00 +0700

    change field post response

--- basic/resources/Post.php
+++ basic/resources/Post.php
@@ -4,11 +4,17 @@ namespace app\resources;
 
 class Post extends \app\models\Post
 {
-	public function fields(){
-		return ['id', 'title', 'body'];
-	}
+	// public function fields(){
+	// 	return ['id', 'title', 'body'];
+	// }
 
 	public function extraFields(){
-		return ['created_at', 'updated_at', 'updated_by', 'created_by'];
+		//return ['comments', 'created_at', 'updated_at', 'updated_by', 'created_by'];
+		return ['comments', 'created_by'];
+	}
+
+	public function getComments()
+	{
+		return $this->hasMany(Comment::class, ['post_id' => 'id']);
 	}
 }
