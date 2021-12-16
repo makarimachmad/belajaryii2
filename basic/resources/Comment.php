4e97887bd632bc31414bbe8dca0f2131114607ed
achmad.widyanto <achmad.widyanto@spesolution.com>
2021-12-16 08:54:44 +0700

    change field comment response

--- basic/resources/Comment.php
+++ basic/resources/Comment.php
@@ -4,11 +4,16 @@ namespace app\resources;
 
 class Comment extends \app\models\Comment
 {
-	public function fields(){
-		return ['id', 'title', 'body'];
-	}
+	// public function fields(){
+	// 	return ['id', 'title', 'body', 'post_id'];
+	// }
 
 	public function extraFields(){
-		return ['post_id', 'created_at', 'updated_at', 'updated_by', 'created_by'];
+		//return ['post_id', 'created_at', 'updated_at', 'updated_by', 'created_by'];
+		return ['post'];
+	}
+
+	public function getPost(){
+		return $this->hasOne(Post::class, ['id' => 'post_id']);
 	}
 }
