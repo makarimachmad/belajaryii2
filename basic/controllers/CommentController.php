1790d50bedb35d07e5d1592f9b0d5deb4433c86f
achmad.widyanto <achmad.widyanto@spesolution.com>
2021-12-16 08:53:25 +0700

    add rule authentication methods on comment

--- basic/controllers/CommentController.php
+++ basic/controllers/CommentController.php
@@ -4,8 +4,35 @@ namespace app\controllers;
 
 use app\resources\Comment;
 use yii\rest\ActiveController;
+use yii\data\ActiveDataProvider;
+use yii\filters\auth\HttpBearerAuth;
 
 class CommentController extends ActiveController
 {
     public $modelClass = Comment::class;
+
+    public function behaviors(){
+        $behaviors = parent::behaviors();
+        $behaviors['authenticator']['only'] = ['create', 'update', 'delete'];
+        $behaviors['authenticator']['authMethods'] = [
+            HttpBearerAuth::class
+        ];
+        
+        return $behaviors;
+    }
+
+    // public function actions()
+    // {
+    //     $actions = parent::actions();
+    //     $actions['index']['prepareDataProvider'] = [$this, 'prepareDataProvider'];
+
+    //     return $actions;
+    // }
+
+    // public function prepareDataProvider()
+    // {
+    //     return new ActiveDataProvider([
+    //         'query' => $this->modelClass::find()->andWhere(['post_id' => \Yii::$app->request->get('postId')])
+    //     ]);
+    // }
 }
