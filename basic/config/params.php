888bacb998e4753823bcdd9bf594091b992d986f
achmad.widyanto <achmad.widyanto@spesolution.com>
2022-01-04 16:11:11 +0700

    declare value for mailer

--- basic/config/params.php
+++ basic/config/params.php
@@ -4,4 +4,7 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
+    'twilioSid' => 'ACce78341cf79b44d393cedc8e0c121497', //replace with your sid
+    'twiliotoken' => '8c341e5839638171b767c2264923891e', //replace with your token
+    'twilioNumber'=>'+6285601403995'//replace with your Twilio phone number
 ];
